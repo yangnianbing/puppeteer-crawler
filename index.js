f27a44708ed0a1e496ebdea14a235b094506aeab
yangnb <yangnb@xiaoshouyi.com>
2017-11-03 18:36:58 +0800

    init

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var puppeteer = require('puppeteer');
+const path = require('path');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
 
 
 var browser = puppeteer.launch({
     headless: false
 })
+
+function createPath(filePath){
+    var sept = filePath.split(path.sep);
+    for(var i = 1, len = sept.length; i < len; i++){
+        sept[i] = path.join(sept[i-1], sept[i]);
+        if(!fs.existsSync(sept[i])){
+            fs.mkdirSync(sept[i]);
+        }
+    }
+}
